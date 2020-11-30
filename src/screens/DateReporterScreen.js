a9f8239a288b7df985c18d9e66ef37c46b33f0b0
Cloudan29 <guitarman.dan29@gmail.com>
2020-11-30 16:37:06 -0500

    date report start screen created and set up

--- src/screens/DateReporterScreen.js
+++ src/screens/DateReporterScreen.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, Text, StyleSheet, TouchableOpacity } from "react-native";
+import CheckBox from '@react-native-community/checkbox'
 
 const DateReporterScreen = function ({ navigation }) {
+  const [myself_selected, setMyself] = useState(false);
+  const [other_selected, setOther] = useState(false);
+
   return <View>
-    <Text>Instructions for the report?</Text>
-    <Button title="Start" onPress={function(){navigation.navigate('Location')}} />
+    <View style={styles.information_container}>
+      <Text style={styles.text_info}>
+        Please fill in the information as best as you can.
+        Anything with a star is obligated, everything else is optional.{"\n\n"}
+        To start, check off what applies below and press start to continue.{"\n"}
+      </Text>
+    </View>
+
+    <View style={styles.form}>
+      <Text style={styles.question_label}>Who did this bad date/aggressor incident happen to?</Text>
+      <View style={styles.checkbox_container}>
+        <CheckBox
+          value={myself_selected}
+          onValueChange={setMyself}
+          style={StyleSheet.checkbox}  
+        />
+        <Text style={styles.label}>Me</Text>
+      </View>
+      <View style={styles.checkbox_container}>
+        <CheckBox
+          value={other_selected}
+          onValueChange={setOther}
+          style={StyleSheet.checkbox}  
+        />
+        <Text style={styles.label}>Someone Else</Text>
+      </View>
+    </View>
+
+    <View style={styles.next_page_button}>
+    <Button 
+      title="Start" 
+      onPress={function(){navigation.navigate('Time')}} 
+      style={styles.next_page_button}
+    />
+    </View>
   </View>
 };
+
+const styles = StyleSheet.create({
+  text_info: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+  question_label: {
+    fontSize: 16,
+  },
+  information_container: {
+    alignItems: "center",
+  },
+  checkbox_container: {
+    flexDirection: "row",
+  },
+  label: {
+    margin: 8,
+  },
+  form: {
+    flexDirection: "column"
+  },
+  next_page_button: {
+    paddingTop: 50,
+    alignSelf: "center",
+    width: 100,
+  }
+})
 
 export default DateReporterScreen;
